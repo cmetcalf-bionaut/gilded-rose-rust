775964f6388f3cd12a479b6bbff36f8d71d37b8e
cmetcalf-bionaut <cyrus.metcalf@bionautlabs.com>
2022-05-07 06:53:48 -0700

    refactor - continuing to collapse logic disaster

--- src/gildedrose.rs
+++ src/gildedrose.rs
@@ -53,38 +53,33 @@ impl GildedRose {
     pub fn update_quality(&mut self) {
         for item in &mut self.items {
             if item.name != "Sulfuras, Hand of Ragnaros" {
-                if item.name == "Aged Brie"
-                    || item.name == "Backstage passes to a TAFKAL80ETC concert"
+                if item.name != "Aged Brie"
+                    && item.name != "Backstage passes to a TAFKAL80ETC concert"
                 {
+                    item.update();
+                } else {
                     if item.quality < 50 {
                         item.quality += 1;
 
                         if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                            if item.sell_in < 11 {
-                                if item.quality < 50 {
-                                    item.quality += 1;
-                                }
+                            if item.sell_in < 11 && item.quality < 50 {
+                                item.quality += 1;
                             }
 
-                            if item.sell_in < 6 {
-                                if item.quality < 50 {
-                                    item.quality += 1;
-                                }
+                            if item.sell_in < 6 && item.quality < 50 {
+                                item.quality += 1;
                             }
                         }
                     }
                     item.sell_in -= 1;
-                } else {
-                    item.update();
-                }
-
-                if item.sell_in < 0 {
-                    if item.name != "Aged Brie" {
-                        if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                            item.quality = 0;
+                    if item.sell_in < 0 {
+                        if item.name != "Aged Brie" {
+                            if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                                item.quality = 0;
+                            }
+                        } else if item.quality < 50 {
+                            item.quality += item.quality + 1;
                         }
-                    } else if item.quality < 50 {
-                        item.quality += item.quality + 1;
                     }
                 }
             }
